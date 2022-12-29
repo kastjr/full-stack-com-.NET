f0a090d5c9b3d08f054a2c6b252b08708fe7dcf8
Kastier Francisco Leite Júnior <108149154+kastjr@users.noreply.github.com>
2022-12-29 18:10:40 -0300

    Add files via upload

---
+++ ProEvents/Back/src/ProEvents.API/Controllers/EventoController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ProEvents.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+
+
+        public EventoController()
+        {
+            
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Exemplo de Get";
+        }
+    }
+
+}
